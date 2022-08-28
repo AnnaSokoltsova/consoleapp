9364c4c03087a34382939f8d78216daf6cb5e6ca
AnnaSokoltsova <107474610+AnnaSokoltsova@users.noreply.github.com>
2022-08-28 16:04:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('How many movies have you seen?', '');
-console.log(numberOfFilms);
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many movies have you seen?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies have you seen?', '');
+    }
+}
+start();
 
 const personaMovieDB = {
     count: numberOfFilms,
@@ -16,17 +24,58 @@ const personaMovieDB = {
     privat: false
 };
 
-const latestMovie = prompt('What is the latest movie you watched?', ''),
-      movieRate = prompt('How would you rate it?', ''),
-      latestMovieA = prompt('What is the latest movie you watched?', ''),
-      movieRateA = prompt('How would you rate it?', '');
-
-let movies = {
-    latestMovie: movieRate
-};
-
-personaMovieDB.movies[latestMovie] = movieRate;
-personaMovieDB.movies[latestMovieA] = movieRateA;
 
 
-console.log(personaMovieDB);
+function detectPersonalLevel() {
+    if (personaMovieDB.count < 10 ) {
+        alert('You have watched not many movies');
+    } else if (personaMovieDB.count >= 10 && personaMovieDB.count < 30) {
+        alert('Your are a classical watcher');
+    } else if (personaMovieDB.count >= 30 ) {
+        alert('You are a movie enjoyer');
+    } else {
+        alert('An error');
+    }
+}
+detectPersonalLevel();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const yourGenre = prompt(`What is your favourite genre under ${i} number?`, '');
+        if (yourGenre != null) {
+            personaMovieDB.genres[i - 1] = yourGenre;
+        } else {
+            console.log('onemoreError');
+            i--;
+        }
+    }
+
+}
+
+writeYourGenres();
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const latestMovie = prompt('What is the latest movie you watched?', '');
+        const movieRate = prompt('How would you rate it?', '');
+        
+        if (latestMovie != null && movieRate != null && latestMovie != "" && movieRate != "" && latestMovie.length < 50) {
+            personaMovieDB.movies[latestMovie] = movieRate; 
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personaMovieDB);
+        console.log('yay');
+    }
+}
+
+showMyDB(personaMovieDB.privat);
+
